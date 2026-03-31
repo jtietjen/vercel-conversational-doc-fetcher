9105354bfcc338d92950814cacc93d5ffbaba908
Jan Tietjens <jan.tietjens@kuehne-nagel.com>
2026-03-31 16:27:09 +0200

    Fix Redis client to support Vercel KV env var names

--- lib/kv.ts
+++ lib/kv.ts
@@ -1,6 +1,9 @@
 import { Redis } from '@upstash/redis';
 
-const kv = Redis.fromEnv();
+const kv = new Redis({
+  url: (process.env.UPSTASH_REDIS_REST_URL ?? process.env.KV_REST_API_URL)!,
+  token: (process.env.UPSTASH_REDIS_REST_TOKEN ?? process.env.KV_REST_API_TOKEN)!,
+});
 import type { ConversationMessage, ConversationState } from '@/types';
 
 // KV TTL: 7 days in seconds
